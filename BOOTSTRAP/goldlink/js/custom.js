5d0187fedb456830e10bc880f647039c23f6b185
jamil hasan <jamil8305@gmail.com>
2020-12-09 18:46:52 +0600

    goldlink

---
+++ BOOTSTRAP/goldlink/js/custom.js
@@ -0,0 +1,16 @@
+$('.ttmnl_wrapper .owl-carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:1
+        }
+    }
+});
